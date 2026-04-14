bf5540508fc25552a5991ee5b60e828c4f7f5590
Brandon <bbunnyeng0930@gmail.com>
2026-04-13 23:53:20 -0400

    Setup

---
+++ MY_Spodify_UI/src/MyApp/main.java
@@ -0,0 +1,21 @@
+package MyApp;
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+
+public class main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        // Your JavaFX application code here
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
